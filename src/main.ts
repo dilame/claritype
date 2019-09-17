6b46a645526cbc0b5719ec2ffb1b45b601cbc59f
bowzee <dilame.nickolson@gmail.com>
2019-09-17 15:55:49 +0300

    Initial

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export type Milliseconds = number;
+export type Seconds = number;
+export type Minutes = number;
+export type Hours = number;
+export type Days = number;
+export type Months = number;
+export type Years = number;
+export type Centuries = number;
